f11e6149031ef38e5c06f4f4719ad8b6e6a39b02
Petros Giannopoulos <pegiannos@gmail.com>
2023-05-23 17:59:57 +0300

    Objective Events

---
+++ Nine Hells/Assets/_Scripts/LevelSpecific/Hell2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hell2 : MonoBehaviour
+{
+    public void OnNpcHelped()
+    {
+        Debug.Log("Done!");
+    }
+}
